436a43ce4ff7f077fcf8b80e6ceb88082ae7156d
Lucas <lucass@gmail.com>
2015-07-16 02:10:38 -0300

    Calling magnet download directly to see what will happen

--- src/main/java/torrent/JavaTorrent.java
+++ src/main/java/torrent/JavaTorrent.java
@@ -70,5 +70,44 @@ public class JavaTorrent extends Thread {
 			frame.dispose();
 		}
 	}
+	
+	private static boolean alreadyStarted = false;
+	private static TorrentManager torrentManagerLazy;
+	private static TrackerManager trackerManagerLazy;
+	public static void downloadMagnet(String magnetUri, String outputFolder){
+		if(!alreadyStarted){			
+			log = ConsoleLogger.createLogger("JavaTorrent", Level.INFO);
+			Config.getConfig().load();
+			Config.getConfig().setDefault("peer-max", 500);
+			Config.getConfig().setDefault("peer-max_burst_ratio", 1.5F);
+			Config.getConfig().setDefault("peer-max_concurrent_connecting", 2);
+			Config.getConfig().setDefault("peer-max_connecting", 50);
+			Config.getConfig().setDefault("download-output_folder", outputFolder);
+			Config.getConfig().setDefault("download-port", 6881);
+			Config.getConfig().setDefault("general-show_all_peers", false);
+			
+			// Initialise reserved bytes field
+			RESERVED_EXTENTION_BYTES[5] |= 0x10; // Extended Messages
+			
+			// Initialise managers
+			torrentManagerLazy = new TorrentManager();
+			trackerManagerLazy = new TrackerManager();		
+			
+			Thread trackerManagerThread = new Thread(trackerManagerLazy, "Tracker manager");
+			trackerManagerThread.setDaemon(true);
+			trackerManagerThread.start();
+			
+			torrentManagerLazy.startListener(trackerManagerLazy);
+			alreadyStarted = true;
+		}
+		
+		MagnetLink magnet = new MagnetLink(magnetUri, torrentManagerLazy, trackerManagerLazy);
+		if (!magnet.isDownloadable()) {
+			log.severe("Magnet link error occured");
+			return;
+		}
+		Torrent torrent = magnet.getTorrent();
+		torrent.start();
+	}
 
 }
